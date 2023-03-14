4b9c073663af3e16842617e10744493d33a23e3d
gersonbarajas62 <gersonbarajas62@gmail.com>
2023-03-13 19:05:42 -0600

    form, add new task and build folder completed

--- src/App.js
+++ src/App.js
@@ -53,8 +53,11 @@ function App() {
   return (
     <div className="container">
       
-      <Header onAdd={() => setShowAddtask(!showAddTask)}/>
+      <Header onAdd={() => setShowAddtask
+        (!showAddTask)} showAdd={showAddTask}/>
       {showAddTask && <AddTask onAdd={addTask}/>}
+
+
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} 
         onDelete={deleteTask} onToggle={toggleReminder}/>) :
